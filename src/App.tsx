f7c80c83b975e43a79cca203e1e6f2d2e5988863
Giovanny Garzón Soto <giovannygarzonsoto@gmail.com>
2024-05-15 17:33:50 -0400

    MangaApp React is coming!

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './index.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-1xl text-tahiti font-bold underline">
+      MangaApp in React is coming!
+    </h1>
+    </>
+  )
+}
+
+export default App
